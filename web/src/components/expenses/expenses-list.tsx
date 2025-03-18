e0e4d5d189083f798bcd8f8e8a851553c026fb20
Valerio Mattera <matteravalerio@gmail.com>
2025-03-18 08:08:22 +0000

    Fixed mobile style

--- web/src/components/expenses/expenses-list.tsx
+++ web/src/components/expenses/expenses-list.tsx
@@ -16,32 +16,36 @@ export const ExpensesList = ({ expenses }: { expenses?: Expense[] }) => {
   }
 
   return (
-    <Table.Root borderRadius={8} lg={{ fontSize: 'md' }} overflow="hidden" p={4} showColumnBorder>
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader w={44}>Date</Table.ColumnHeader>
-          <Table.ColumnHeader w={96}>Title</Table.ColumnHeader>
-          <Table.ColumnHeader>Description</Table.ColumnHeader>
-          <Table.ColumnHeader w={44} textAlign="end">
-            Amount
-          </Table.ColumnHeader>
-          <Table.ColumnHeader w={0}></Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {expenses.map((expense) => (
-          <Table.Row key={expense.id} cursor="pointer" position="relative">
-            <Table.Cell>{formatDate(expense.date)}</Table.Cell>
-            <Table.Cell>{expense.title}</Table.Cell>
-            <Table.Cell textOverflow="ellipsis">{expense.description}</Table.Cell>
-            <Table.Cell textAlign="end">£{expense.amount.toFixed(2)}</Table.Cell>
-            <Table.Cell textAlign="end" display="flex" gap={2}>
-              <UpdateExpenseModal expense={expense} />
-              <DeleteExpenseModal id={expense.id} />
-            </Table.Cell>
+    <Table.ScrollArea maxW="full">
+      <Table.Root borderRadius={8} lg={{ fontSize: 'md' }} overflow="hidden" p={4} showColumnBorder>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader w={44}>Date</Table.ColumnHeader>
+            <Table.ColumnHeader w={96}>Title</Table.ColumnHeader>
+            <Table.ColumnHeader>Description</Table.ColumnHeader>
+            <Table.ColumnHeader w={44} textAlign="end">
+              Amount
+            </Table.ColumnHeader>
+            <Table.ColumnHeader w={0}></Table.ColumnHeader>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+        <Table.Body>
+          {expenses.map((expense) => (
+            <Table.Row key={expense.id} cursor="pointer" position="relative">
+              <Table.Cell>{formatDate(expense.date)}</Table.Cell>
+              <Table.Cell>{expense.title}</Table.Cell>
+              <Table.Cell textOverflow="ellipsis">{expense.description}</Table.Cell>
+              <Table.Cell textAlign="end">£{expense.amount.toFixed(2)}</Table.Cell>
+              <Table.Cell textAlign="end">
+                <Box as="span" display="inline-flex" gap={2} aria-label="Edit and delete expense">
+                  <UpdateExpenseModal expense={expense} />
+                  <DeleteExpenseModal id={expense.id} />
+                </Box>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Table.ScrollArea>
   )
 }
